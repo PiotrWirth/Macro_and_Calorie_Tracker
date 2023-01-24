359dd058850316f91c2d222f99e0b3df3ac5faa3
Piotr <piotr.wirth@onet.pl>
2023-01-24 19:14:44 +0100

    commit

--- mytracker/myapp/views.py
+++ mytracker/myapp/views.py
@@ -6,17 +6,26 @@ from .models import Food, Consume
 
 def index(request):
 
-    if request.method =="POST":
+    if request.method == "POST":
         food_consumed = request.POST['food_consumed']
         consume = Food.objects.get(name=food_consumed)
         user = request.user
-        consume = Consume(user=user,food_consumed=consume)
+        consume = Consume(user=user, food_consumed=consume)
         consume.save()
         foods = Food.objects.all()
         return HttpResponseRedirect('/')
     else:
         foods = Food.objects.all()
     consumed_food = Consume.objects.filter(user=request.user)
- 
-    
-    return render(request,'myapp/index.html',{'foods':foods,'consumed_food':consumed_food})
+
+    return render(request, 'myapp/index.html', {'foods': foods, 'consumed_food': consumed_food})
+
+
+def delete_consume(request, id):
+
+    consumed_food = Consume.objects.get(id=id)
+    if request.method == 'POST':
+        consumed_food.delete()
+        return HttpResponseRedirect('/')
+    else:
+        return render(request, 'myapp/delete.html')
